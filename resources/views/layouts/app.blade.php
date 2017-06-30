afaae372640b49ade82eabd9f78bfc1d451150a6
TranDieuLinh <linhttd.bk@gmail.com>
2017-06-30 10:56:49 +0700

    final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href ="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('bower_components/font-awesome/css/font-awesome.min.css') }}">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/default.css') }}" rel="stylesheet">
-    {{--<link href="{{ asset('css/food-details.css') }}" rel="stylesheet">--}}
-    @yield('css')
-    <!-- Scripts -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{ Html::style('css/app.css') }}
+    {{ Html::style('css/default.css') }}
+{{--<link href="{{ asset('css/food-details.css') }}" rel="stylesheet">--}}
+@yield('css')
+<!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
@@ -25,14 +25,15 @@
     </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="topNav">
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="topNav">
             <div class="container">
                 <div class="navbar-header alignR">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                            data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -59,15 +60,22 @@
                     <ul class="nav navbar-nav navbar-right topNav1">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a class="item-cart" data-toggle="modal" data-target="#squarespaceModal"><i class="fa fa-shopping-cart fa-fw"></i><span class="cart-count">{{ Cart::count() }} Item(s) - </span><span class="badge badge-warning money"> {{ Cart::subtotal() }}</span></a></li>
+                            <li><a class="item-cart" data-toggle="modal" data-target="#squarespaceModal"><i
+                                            class="fa fa-shopping-cart fa-fw"></i><span class="cart-count">{{ Cart::count() }}
+                                        Item(s) - </span><span
+                                            class="badge badge-warning money"> {{ Cart::subtotal() }}</span></a></li>
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
 
                         @else
                             <li><a href="{{ url('/home') }}"><i class="fa fa-home fa-fw"></i> Home</a></li>
-                            <li><a class="item-cart" data-toggle="modal" data-target="#squarespaceModal"><i class="fa fa-shopping-cart fa-fw"></i><span class="cart-count">{{ Cart::count() }} Item(s) - </span><span class="badge badge-warning money"> {{ Cart::subtotal() }}</span></a></li>
+                            <li><a class="item-cart" data-toggle="modal" data-target="#squarespaceModal"><i
+                                            class="fa fa-shopping-cart fa-fw"></i><span class="cart-count">{{ Cart::count() }}
+                                        Item(s) - </span><span
+                                            class="badge badge-warning money"> {{ Cart::subtotal() }}</span></a></li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle dropdown-custom" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" class="dropdown-toggle dropdown-custom" data-toggle="dropdown" role="button"
+                                   aria-expanded="false">
                                     <i class="fa fa-user fa-fw"></i>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -75,12 +83,13 @@
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
+                                           onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
@@ -90,56 +99,63 @@
                     </ul>
                 </div>
             </div>
-            </div>
-        </nav>
-        @if (session('message'))
-            <div class="alert alert-{{ session('status') }}">
-                {{ session('message') }}
-            </div>
-        @endif
-        @yield('content')
+        </div>
+    </nav>
+    @if (session('message'))
+        <div class="alert alert-{{ session('status') }}">
+            {{ session('message') }}
+        </div>
+@endif
+@yield('content')
 
 
-    <!-- line modal -->
-        <div class="modal fade" id="squarespaceModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
-                        <h3 class="modal-title" id="lineModalLabel">Products in the cart</h3>
-                    </div>
-                    @php($rows = \Gloudemans\Shoppingcart\Facades\Cart::content())
-                    @if($rows)
-                        <div id="cart_content">
-                    @foreach($rows as $row)
+<!-- line modal -->
+    <div class="modal fade" id="squarespaceModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span
+                                class="sr-only">Close</span></button>
+                    <h3 class="modal-title" id="lineModalLabel">Products in the cart</h3>
+                </div>
+                @php($rows = \Gloudemans\Shoppingcart\Facades\Cart::content())
+                @if($rows)
+                    <div id="cart_content">
+                        @foreach($rows as $row)
                             @include ('layouts.cart-item')
-                    @endforeach
+                        @endforeach
+                    </div>
+                    <a href="{{ route('order') }}" style="margin-left: 20px" class="btn btn-success">By Now</a>
+                @endif
+                <div class="modal-footer">
+                    <div class="btn-group btn-group-justified" role="group" aria-label="group button">
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-default" data-dismiss="modal" role="button">Close
+                            </button>
                         </div>
-                        <a href="{{ route('order') }}" style="margin-left: 20px" class="btn btn-success">By Now</a>
-                    @endif
-                    <div class="modal-footer">
-                        <div class="btn-group btn-group-justified" role="group" aria-label="group button">
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-default" data-dismiss="modal"  role="button">Close</button>
-                            </div>
-                            <div class="btn-group btn-delete hidden" role="group">
-                                <button type="button" id="delImage" class="btn btn-default btn-hover-red" data-dismiss="modal"  role="button">Delete</button>
-                            </div>
-                            <div class="btn-group" role="group">
-                                <button type="button" id="saveImage" class="btn btn-default btn-hover-green" data-action="save" role="button">Save</button>
-                            </div>
+                        <div class="btn-group btn-delete hidden" role="group">
+                            <button type="button" id="delImage" class="btn btn-default btn-hover-red"
+                                    data-dismiss="modal" role="button">Delete
+                            </button>
+                        </div>
+                        <div class="btn-group" role="group">
+                            <button type="button" id="saveImage" class="btn btn-default btn-hover-green"
+                                    data-action="save" role="button">Save
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
+</div>
+</div>
 
-    <!-- Scripts -->
-    <script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/default.js') }}"></script>
-    @yield('script')
+<!-- Scripts -->
+<script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.js') }}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/default.js') }}"></script>
+@yield('script')
 </body>
 </html>
